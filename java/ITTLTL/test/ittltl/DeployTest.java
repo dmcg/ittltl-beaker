bb6b06e4e93ce91ffe06520b6f7961bcc7736975
Duncan McGregor <duncan@oneeyedmen.com>
2013-06-17 15:41:22 +0100

    Add FlashTest.java, rename DelmeTest to DeployTest

--- java/ITTLTL/test/ittltl/DeployTest.java
+++ java/ITTLTL/test/ittltl/DeployTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class DelmeTest {
+public class DeployTest {
 
 	@Test
 	public void test() {
---
+++ java/ITTLTL/test/ittltl/FlashTest.java
@@ -0,0 +1,14 @@
+package ittltl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class FlashTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
