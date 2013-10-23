398bc9c1a9807f909777ae330e77e4ca78207499
Duncan McGregor <duncan@oneeyedmen.com>
2013-10-23 11:52:13 +0100

    actual paths for first test

--- script.sh
+++ script.sh
@@ -5,7 +5,8 @@ echo "It's not yet time to light the lights"
 ### Following fragment shows how to run java assuming class files in bin sub-directory
 #cd java/ITTLTL
 #./jrun package.ClassName args...
-#./jrun org.junit.runner.JUnitCore package.TestClass
+#./jrun org.junit.runner.JUnitCore ittltl.DeployTest
 
 ### Following fragment shows how to run python
-#sudo python python/python-script args...
+#cd python
+#python test/deploy_test.py
