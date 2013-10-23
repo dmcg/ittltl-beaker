dcfe2abec858703d02a473ff75b9620de30563e0
Richard Care <richard@octaveassociates.co.uk>
2013-10-23 10:26:24 +0100

    Add example script for cgi to call

---
+++ script.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+### Following fragment shows how to run java assuming class files in bin sub-directory
+#cd java/ITTLTL
+#./jrun package.ClassName args...
+#./jrun org.junit.runner.JUnitCore package.TestClass
+
+### Following fragment shows how to run python
+#sudo python python/python-script args... <<HERE
+#ittltl
+#HERE
