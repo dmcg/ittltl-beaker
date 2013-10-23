c8446472651cb3ed11a17fad8dd93444db276649
Duncan McGregor <duncan@oneeyedmen.com>
2013-10-23 11:31:49 +0100

    fix permissions

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
+echo "It's not yet time to light the lights"
+
 ### Following fragment shows how to run java assuming class files in bin sub-directory
 #cd java/ITTLTL
 #./jrun package.ClassName args...
 #./jrun org.junit.runner.JUnitCore package.TestClass
 
 ### Following fragment shows how to run python
-#sudo python python/python-script args... <<HERE
-#ittltl
-#HERE
+#sudo python python/python-script args...
